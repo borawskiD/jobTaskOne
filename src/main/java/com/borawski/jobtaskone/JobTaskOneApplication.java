fca1d0d1569b0026162830b7e967459353a98c07
borawskiD <borawskid@icloud.com>
2023-11-14 20:56:32 +0100

    Fix docker config

--- src/main/java/com/borawski/jobtaskone/JobTaskOneApplication.java
+++ src/main/java/com/borawski/jobtaskone/JobTaskOneApplication.java
@@ -1,9 +1,12 @@
 package com.borawski.jobtaskone;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
+
 public class JobTaskOneApplication {
 
     public static void main(String[] args) {
