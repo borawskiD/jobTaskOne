088239b104ffda40c6976b2ed7d520ff39e0c3be
borawskiD <borawskid@icloud.com>
2023-11-14 15:20:45 +0100

    Task is ready, state before refactor

--- src/main/java/com/borawski/jobtaskone/TransferRepository.java
+++ src/main/java/com/borawski/jobtaskone/TransferRepository.java
@@ -2,7 +2,11 @@ package com.borawski.jobtaskone;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDate;
 
 @Repository
 public interface TransferRepository extends ReactiveMongoRepository<TransferCollection, String> {
+    Mono<TransferCollection> findByTransactionDateAndSenderAccountNumber(LocalDate transactionDate, String senderAccountNumber);
 }
